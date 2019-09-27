0e500a32583dafe94461c3f6fc307649e93fc340
Emyrk <stevenmasley@gmail.com>
2019-09-26 21:16:19 -0500

    Add Factoid Transactions
    Included factoid transactions marshal/unmarshal. Not included
    is the composing of factoid transactions.

---
+++ transaction.go
@@ -0,0 +1,403 @@
+package factom
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/binary"
+	"fmt"
+	"time"
+
+	"github.com/Factom-Asset-Tokens/factom/varintf"
+)
+
+type FactoidTransaction struct {
+	// TODO: The header is usually at the top level, is this ok?
+	FactoidTransactionHeader
+
+	FCTInputs  []FactoidTransactionIO        `json:"inputs"`
+	FCTOutputs []FactoidTransactionIO        `json:"fctoutputs"`
+	ECOutputs  []FactoidTransactionIO        `json:"ecoutputs"`
+	Signatures []FactoidTransactionSignature `json:"signatures"`
+}
+
+type FactoidTransactionHeader struct {
+	TransactionID *Bytes32 `json:"txid"`
+
+	Version uint64 `json:"version"`
+	// Timestamp is accurate to the millisecond
+	Timestamp time.Time `json:"timestamp"`
+
+	InputCount     uint8 `json:"inputcount"`
+	FCTOutputCount uint8 `json:"fctoutcount"`
+	ECOutputCount  uint8 `json:"ecoutcount"`
+}
+
+type FactoidTransactionIOs []FactoidTransactionIO
+
+type FactoidTransactionIO struct {
+	Amount uint64 `json:"amount"`
+	// Address can be an SHA256d(RCD) for FCT in/out, or a public key for EC out.
+	// It is the encoded bytes into the human readable addresses
+	Address *Bytes32 `json:"address"`
+}
+
+type FactoidTransactionSignature struct {
+	// SHA256d(RCD) == FactoidIOAddress for the inputs
+	ReedeemCondition RCD   `json:"rcd"`
+	SignatureBlock   Bytes `json:"amount"`
+}
+
+// IsPopulated returns true if f has already been successfully populated by a
+// call to Get. IsPopulated returns false if f.FCTInputs, or f.Signatures are
+// nil, or if f.Timestamp is zero.
+func (f FactoidTransaction) IsPopulated() bool {
+	return f.FCTInputs != nil && // Although a coinbase has 0 inputs, this array should not be nil
+		f.Signatures != nil &&
+		f.Timestamp != time.Time{}
+}
+
+// IsPopulated returns true if io has already been successfully populated by a
+// call to Get. IsPopulated returns false if io.Address is nil
+func (io FactoidTransactionIO) IsPopulated() bool {
+	return io.Address != nil
+}
+
+// IsPopulated returns true if s has already been successfully populated by a
+// call to Get. IsPopulated returns false if s.SignatureBlock or s.ReedeemCondition are nil
+func (s FactoidTransactionSignature) IsPopulated() bool {
+	return s.SignatureBlock != nil &&
+		s.ReedeemCondition != nil
+}
+
+// Get queries factomd for the entry corresponding to f.TransactionID, which must be not
+// nil. After a successful call all inputs, outputs, and the header will be populated
+func (f *FactoidTransaction) Get(c *Client) error {
+	// TODO: Test this functionality
+	// If the TransactionID is nil then we have nothing to query for.
+	if f.TransactionID == nil {
+		return fmt.Errorf("txid is nil")
+	}
+	// If the Transaction is already populated then there is nothing to do. If
+	// the Hash is nil, we cannot populate it anyway.
+	if f.IsPopulated() {
+		return nil
+	}
+	requestTxId := f.TransactionID
+
+	params := struct {
+		Hash *Bytes32 `json:"hash"`
+	}{Hash: f.TransactionID}
+	var result struct {
+		Data Bytes `json:"data"`
+	}
+	if err := c.FactomdRequest("raw-data", params, &result); err != nil {
+		return err
+	}
+
+	// TODO: handle the response bytes
+	if err := f.UnmarshalBinary(result.Data); err != nil {
+		return err
+	}
+
+	txid, err := f.ComputeTransactionID()
+	if err != nil {
+		return err
+	}
+	if txid != *requestTxId {
+		return fmt.Errorf("invalid txid")
+	}
+
+	return nil
+}
+
+// ComputeTransactionID computes the txid for a given transaction. The txid is the sha256 of
+// the ledger fields in a factoid transaction. The ledger fields exclude the signature block of
+// the transaction
+func (f *FactoidTransaction) ComputeTransactionID() (Bytes32, error) {
+	data, err := f.MarshalLedgerBinary()
+	if err != nil {
+		return Bytes32{}, err
+	}
+
+	txid := Bytes32(sha256.Sum256(data))
+	f.TransactionID = &txid
+	return txid, nil
+}
+
+// ComputeFullHash computes the fullhash for a given transaction. The fullhash is the sha256 of all
+// the fields in a factoid transaction.
+func (f *FactoidTransaction) ComputeFullHash() (*Bytes32, error) {
+	data, err := f.MarshalBinary()
+	if err != nil {
+		return nil, err
+	}
+
+	txid := Bytes32(sha256.Sum256(data))
+	return &txid, nil
+}
+
+// MarshalLedgerBinary marshals the transaction ledger fields to their binary representation.
+// This exludes the signature blocks
+func (f *FactoidTransaction) MarshalLedgerBinary() ([]byte, error) {
+	// TODO: More checks up front?
+	if !f.IsPopulated() {
+		return nil, fmt.Errorf("not populated")
+	}
+
+	// It's very difficult to know the size before marshaling, as each in/out has a varint
+	// so make the buffer at the end
+
+	// The header bytes
+	header, err := f.MarshalHeaderBinary()
+	if err != nil {
+		return nil, err
+	}
+
+	// Inputs
+	inputs, err := FactoidTransactionIOs(f.FCTInputs).MarshalBinary()
+	if err != nil {
+		return nil, err
+	}
+
+	// FCT Outputs
+	fctout, err := FactoidTransactionIOs(f.FCTOutputs).MarshalBinary()
+	if err != nil {
+		return nil, err
+	}
+
+	// EC Outputs
+	ecout, err := FactoidTransactionIOs(f.ECOutputs).MarshalBinary()
+	if err != nil {
+		return nil, err
+	}
+
+	data := make([]byte, len(header)+len(inputs)+len(fctout)+len(ecout))
+	i := 0
+	i += copy(data[i:], header)
+	i += copy(data[i:], inputs)
+	i += copy(data[i:], fctout)
+	i += copy(data[i:], ecout)
+
+	return data, nil
+}
+
+// TODO: Re-eval how to do this. Kinda different from the rest
+func (f *FactoidTransaction) MarshalBinary() ([]byte, error) {
+	// TODO: More checks up front?
+	if !f.IsPopulated() {
+		return nil, fmt.Errorf("not populated")
+	}
+
+	data, err := f.MarshalLedgerBinary()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, s := range f.Signatures {
+		sig, err := s.MarshalBinary()
+		if err != nil {
+			return nil, err
+		}
+		data = append(data, sig...)
+	}
+
+	return data, nil
+}
+
+// MarshalHeaderBinary marshals the transaction's header to its binary representation. See
+// UnmarshalHeaderBinary for encoding details.
+func (f *FactoidTransaction) MarshalHeaderBinary() ([]byte, error) {
+	version := varintf.Encode(f.Version)
+	data := make([]byte, TransactionHeadMinLen+len(version))
+	i := 0
+	i += copy(data[i:], version)
+
+	// Do the timestamp as 6 bytes in ms
+	ms := f.Timestamp.UnixNano() / 1e6
+	buf := bytes.NewBuffer(make([]byte, 0, 8))
+	if err := binary.Write(buf, binary.BigEndian, ms); err != nil {
+		return nil, err
+	}
+	i += copy(data[i:], buf.Bytes()[2:])
+
+	data[i] = f.InputCount
+	i += 1
+	data[i] = f.FCTOutputCount
+	i += 1
+	data[i] = f.ECOutputCount
+	i += 1
+	return data, nil
+}
+
+// MarshalBinary marshals a set of transaction ios to its binary representation. See
+// UnmarshalBinary for encoding details.
+func (ios FactoidTransactionIOs) MarshalBinary() ([]byte, error) {
+	var data []byte
+	for _, io := range ios {
+		iodata, err := io.MarshalBinary()
+		if err != nil {
+			return nil, err
+		}
+		data = append(data, iodata...)
+	}
+	return data, nil
+}
+
+// MarshalBinary marshals a transaction io to its binary representation. See
+// UnmarshalBinary for encoding details.
+func (io *FactoidTransactionIO) MarshalBinary() ([]byte, error) {
+	if !io.IsPopulated() {
+		return nil, fmt.Errorf("not populated")
+	}
+
+	amount := varintf.Encode(io.Amount)
+	data := make([]byte, 32+len(amount))
+	i := 0
+	i += copy(data[i:], amount)
+	i += copy(data[i:], io.Address[:])
+	return data, nil
+}
+
+// MarshalBinary marshals a transaction signature to its binary representation. See
+// UnmarshalBinary for encoding details.
+func (s *FactoidTransactionSignature) MarshalBinary() ([]byte, error) {
+	if !s.IsPopulated() {
+		return nil, fmt.Errorf("not populated")
+	}
+
+	rcdData, err := s.ReedeemCondition.MarshalBinary()
+	if err != nil {
+		return nil, err
+	}
+
+	data := make([]byte, len(rcdData)+len(s.SignatureBlock))
+	i := 0
+	i += copy(data[i:], rcdData)
+	i += copy(data[i:], s.SignatureBlock)
+	return data, nil
+}
+
+const (
+	TransactionHeadMinLen = 0 + // Version length is varint
+		6 + // timestamp
+		1 + // input count
+		1 + // factoid output count
+		1 // EC output count
+
+)
+
+func (f *FactoidTransaction) UnmarshalBinary(data []byte) error {
+	// TODO: Some length checks to prevent too few/too many bytes
+
+	// Decode header
+	i, err := f.DecodeHeader(data)
+	if err != nil {
+		return err
+	}
+
+	// Decode the inputs
+	f.FCTInputs = make([]FactoidTransactionIO, f.InputCount)
+	read, err := FactoidTransactionIOs(f.FCTInputs).Decode(data[i:])
+	if err != nil {
+		return err
+	}
+	i += read
+
+	// Decode the FCT Outputs
+	f.FCTOutputs = make([]FactoidTransactionIO, f.FCTOutputCount)
+	read, err = FactoidTransactionIOs(f.FCTOutputs).Decode(data[i:])
+	if err != nil {
+		return err
+	}
+	i += read
+
+	// Decode the EC Outputs
+	f.ECOutputs = make([]FactoidTransactionIO, f.ECOutputCount)
+	read, err = FactoidTransactionIOs(f.ECOutputs).Decode(data[i:])
+	if err != nil {
+		return err
+	}
+	i += read
+
+	// Decode the signature blocks, one per input
+	f.Signatures = make([]FactoidTransactionSignature, f.InputCount)
+	for c := uint8(0); c < f.InputCount; c++ {
+		// f.Signatures[i] = new(FactoidTransactionSignature)
+		read, err := f.Signatures[c].Decode(data[i:])
+		if err != nil {
+			return err
+		}
+		i += read
+	}
+
+	txid, err := f.ComputeTransactionID()
+	if err != nil {
+		return err
+	}
+	f.TransactionID = &txid
+
+	return nil
+}
+
+func (f *FactoidTransaction) DecodeHeader(data []byte) (int, error) {
+	version, i := varintf.Decode(data)
+	f.Version = version
+
+	msdata := make([]byte, 8)
+	copy(msdata[2:], data[i:i+6])
+	ms := binary.BigEndian.Uint64(msdata)
+	f.Timestamp = time.Unix(0, int64(ms)*1e6)
+	i += 6
+	f.InputCount = data[i]
+	i += 1
+	f.FCTOutputCount = data[i]
+	i += 1
+	f.ECOutputCount = data[i]
+	i += 1
+
+	return i, nil
+}
+
+// Decode takes a given input and decodes the set of bytes needed to populate
+// the set of factoid transactions ios. The set length should be preset before
+// calling this function. It will return how many bytes it read and return an error.
+func (ios FactoidTransactionIOs) Decode(data []byte) (int, error) {
+	i := 0
+	for c := 0; c < len(ios); c++ {
+		read, err := ios[c].Decode(data[i:])
+		if err != nil {
+			return 0, err
+		}
+
+		i += read
+	}
+
+	return i, nil
+}
+
+// Decode takes a given input and decodes the set of bytes needed for a full
+// transaction input/output. It will return how many bytes it read and an error.
+// A FactoidTransactionIO includes an amount and an address.
+func (io *FactoidTransactionIO) Decode(data []byte) (int, error) {
+	amount, i := varintf.Decode(data)
+	io.Amount = amount
+	io.Address = NewBytes32(data[i : i+32])
+	i += 32
+
+	return i, nil
+}
+
+// Decode will take a given input and decode the set of bytes needed for the full
+// FactoidTransactionSignature. It will return how many bytes it read and an error.
+// A FactoidTransactionSignature includes the RCD type and it's signature block.
+func (s *FactoidTransactionSignature) Decode(data []byte) (int, error) {
+	rcd, i, err := DecodeRCD(data)
+	if err != nil {
+		return 0, err
+	}
+	s.ReedeemCondition = rcd
+	s.SignatureBlock = make([]byte, rcd.SignatureBlockSize())
+	i += copy(s.SignatureBlock, data[i:])
+
+	return i, nil
+}
---
+++ transaction_test.go
@@ -0,0 +1,149 @@
+// MIT License
+//
+// Copyright 2018 Canonical Ledgers, LLC
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+
+package factom_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+
+	. "github.com/Factom-Asset-Tokens/factom"
+	"github.com/stretchr/testify/assert"
+)
+
+var txMarshalBinaryTests = []struct {
+	Name     string
+	TxID     *Bytes32
+	FullHash *Bytes32
+	FactoidTransaction
+}{{
+	Name:     "valid",
+	TxID:     NewBytes32FromString("c7ea8854be1456ed8588b33d8d3cc2d90b911fcb01ec902ca3202e8bdbf269bf"),
+	FullHash: NewBytes32FromString("64251aa63e011f803c883acf2342d784b405afa59e24d9c5506c84f6c91bf18b"),
+	FactoidTransaction: FactoidTransaction{
+		FactoidTransactionHeader: FactoidTransactionHeader{
+			TransactionID:  nil,
+			Version:        2,
+			Timestamp:      time.Unix(0, 1443537161594*1e6),
+			InputCount:     1,
+			FCTOutputCount: 1,
+			ECOutputCount:  0,
+		},
+		FCTInputs: []FactoidTransactionIO{
+			{
+				Amount:  1000000000,
+				Address: NewBytes32FromString("ab87d1b89117aba0e6b131d1ee42f99c6e806b76ca68c0823fb65c80d694b125"),
+			},
+		},
+		FCTOutputs: []FactoidTransactionIO{
+			{
+				Amount:  992000800,
+				Address: NewBytes32FromString("648f374d3de5d1541642c167a34d0f7b7d92fd2dab4d32f313776fa5a2b73a98"),
+			},
+		},
+		ECOutputs: nil,
+		Signatures: []FactoidTransactionSignature{
+			{
+				// RCD 0110560cc304eb0a3b0540bc387930d2a7b2373270cfbd8448bc68a867cefb9f74
+				ReedeemCondition: &RCD1{
+					PublicKey: NewBytes32FromString("10560cc304eb0a3b0540bc387930d2a7b2373270cfbd8448bc68a867cefb9f74"),
+				},
+				SignatureBlock: NewBytesFromString("d68d5ce3bee5e69f113d643df1f6ba0dd476ada40633751537d5b840e2be811d4734ef9f679966fa86b1777c8a387986b4e21987174f9df808c2081be2c04a08"),
+			},
+		},
+	},
+}}
+
+func TestFactoidTransactionMarshalBinary(t *testing.T) {
+	for _, test := range txMarshalBinaryTests {
+		t.Run(test.Name, func(t *testing.T) {
+			f := test.FactoidTransaction
+			txid, err := f.ComputeTransactionID()
+			assert := assert.New(t)
+			assert.NoError(err)
+			assert.Equal(*f.TransactionID, txid)
+			fh, err := f.ComputeFullHash()
+			assert.NoError(err)
+			assert.Equal(fh, test.FullHash)
+		})
+	}
+}
+
+var txUnmarshalBinaryTests = []struct {
+	Name     string
+	Data     []byte
+	Error    string
+	TxID     *Bytes32
+	FullHash *Bytes32
+}{
+	{
+		Name: "valid (1 fct out)",
+		Data: NewBytesFromString(
+			"020162606d234b01010092d097e400304d80538e27505d44d5ff0ada6a9d420d93a9994da75f0763c12c827b61666892d0969560b11e86b4894661091c16f511a2f1000099b54dcf73bc7bcacba6e3fe2f547c83010fd93026041de6387d2dcef0917c06288e690fa7652c20f044746e787b06b2bdf7f1ec53e7e5695667b071b4e3ae65c09c804e164e971ac4717c7a8d0b61053f831f605e0adbf98d19613b00797962beb899a8d9472e187b17444159e74b2f0e"),
+		TxID: NewBytes32FromString(
+			"637d9e8a7464aa272192e499bb0cdc720288884bfd5546bab8dcb8892527fc7b"),
+		FullHash: NewBytes32FromString(
+			"59b293f8ade00f7f51d42762b864e287ea31d195563e1970356fe8c1d49fce97"),
+	},
+	{
+		Name: "valid (1 ec out)",
+		Data: hexToBytes(
+			"02016d16b4aac301000183dd95b22031a14669cd993de61b28454222c75e33f3a3a51eddd8394a150b0068778e9ebc83dd80c2304fe2a4a9debe6bd58e4424c9e1d00dc9e71a23c8c6b9e9db96d478012181cc0801b60844047ee177e4e22bdd48819694fdec025f432f482e820e06d0023befe690e295e854af57c60e8610a28dd743f5427a868f48c10e41e1373c01537d91bea1f0703058e0dd8a5620b81746d6b435f0ddc35daeee496d5dd8f5134fed34490d"),
+		TxID: NewBytes32FromString(
+			"ff61ee20adda5cbb4ab2eb583881e94b707746660900c6a55c11e3a1e5a7de69"),
+		FullHash: NewBytes32FromString(
+			"d8aec7bf1361e2dfeaf7806ab8e00c1bb49a931932abe561fabb0833cddef4c3"),
+	},
+	{
+		Name: "valid (4 fct in, 3 fct out)",
+		Data: hexToBytes(
+			"020154e64948850403008efca5d4105706ce61e91f33eb5506e86a717285a4dbcedb6678e8ffc326a3b11d3aa36123c0dbe8826b53435eb1ee2692ce5a9cb356eba3bfa8dd93f6470551812259d57bd4c13d629292f8b44ed1f8785ae9a13126d566c6cbed3a592b16570d3a7928fe3f6a1782b1caeaee5492e4d7deae75c641be9c4b1ad1d937829c79c397d70c3c2f2745a2d66b9012cfea55bbbba15182dff0d6ff1854053af90420a3c455828e5e7ef5057ebeae2bcf6fb9db62a8de66b1a26e158986b1aaa5f17048d2e5a20571043b492abc1a9d40072c31ee86c0a408b6ef3fd5efc2a7462a328aa3a6dcdb7e8ef427e0978bd09f941a8d2586ce1c6401f2d7663914c6c350555e0bfe32ac010171230c7848588d5de3855fd4d526e302e9c8a309ab591335b5d1d3a071e583f363aee6fbc0ea9bf703644f8199612e2029fdd4f43cfc28d73811228d7b35d88773f59019440e4646b3e8d499cd7e2b1171693dc071543a1e330c22057038ab00012aca1b1cf03adf48039dcca868d93cfe30faa7c931703a3c397b96f846cfb373537273acde3366b8c0d675ce6fd0d3f9f3f0800aaf0afd5b750c482ab12ca52b86fadcb7aba08fb3430f4fab9acf90f2d60daddd9cd9da65143e247339584a0401dd6db3e73e140c28d1f003992ee084623de8b4e955e89c20df73d0d899ad86b0d9d0e5e988031c28879e028ef80301f42fdebb0993dc213a49bc117b24617a151ff200d1d2d907735b7ad90fde18f914e187abaa6c5f4add357113363a67d407012c2df5f3987d34a9ec9a814d6017122a1e78d4569785c7bc8277ed3f8c1e3957ad90bbbd9f2eb774f8e2713a37721a2f7a5e3ae510eacdeeff362e44ade6037a8d33745eced481d9e72527702d5cd887fe19295f0ee204807dc6ce76d1c6ba01"),
+		TxID: NewBytes32FromString(
+			"fa323daccaa9959b27b5a2f4efa5938a3ba9c99e11ef7af6406035d2e06ce286"),
+		FullHash: NewBytes32FromString(
+			"0bc0affe278911b1c59e3ffbe803842787d3d76c3ea42d68250a5b5de5e2b420"),
+	},
+	// TODO: Add invalid tests
+}
+
+func TestFactoidTransaction_UnmarshalBinary(t *testing.T) {
+	for _, test := range txUnmarshalBinaryTests {
+		t.Run("UnmarshalBinary/"+test.Name, func(t *testing.T) {
+			assert := assert.New(t)
+			require := require.New(t)
+			f := FactoidTransaction{}
+			err := f.UnmarshalBinary(test.Data)
+			if len(test.Error) == 0 {
+				require.NoError(err)
+				require.NotNil(f.FCTInputs)
+				hash, err := f.ComputeFullHash()
+				assert.NoError(err)
+				assert.Equal(*test.TxID, *f.TransactionID)
+				assert.Equal(test.FullHash, hash)
+			} else {
+				require.EqualError(err, test.Error)
+			}
+		})
+	}
+}
