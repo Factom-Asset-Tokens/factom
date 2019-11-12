4376ca4325562c803d38287fb3efaf74e0dc3134
Emyrk <stevenmasley@gmail.com>
2019-11-12 17:28:42 -0600

    Correct jsonrpc2 import

--- client_test.go
+++ client_test.go
@@ -6,13 +6,13 @@ import (
 	"io/ioutil"
 	"net/http"
 
-	"github.com/AdamSLevy/jsonrpc2"
+	"github.com/AdamSLevy/jsonrpc2/v12"
 )
 
 // TODO: Where can I stick something like this?
 
-// GetClientWithFixedResp will return a client that no matter what the request is,
-// will always respond with the 'resp' as the body.
+// ClientWithFixedRPCResponse will return a client that no matter what the
+// request is, will always respond with the 'result' as the body.
 // From http://hassansin.github.io/Unit-Testing-http-client-in-Go
 func ClientWithFixedRPCResponse(result interface{}) *http.Client {
 	client := NewTestClient(func(req *http.Request) *http.Response {
@@ -21,9 +21,8 @@ func ClientWithFixedRPCResponse(result interface{}) *http.Client {
 		_ = json.Unmarshal(reqData, &jReq)
 
 		resp := jsonrpc2.Response{
-			JSONRPC: "2.0",
-			Result:  result,
-			ID:      jReq.ID,
+			Result: result,
+			ID:     jReq.ID,
 		}
 
 		respData, _ := json.Marshal(resp)
@@ -40,10 +39,8 @@ func ClientWithFixedRPCResponse(result interface{}) *http.Client {
 	return client
 }
 
-// RoundTripFunc .
 type RoundTripFunc func(req *http.Request) *http.Response
 
-// RoundTrip .
 func (f RoundTripFunc) RoundTrip(req *http.Request) (*http.Response, error) {
 	return f(req), nil
 }
